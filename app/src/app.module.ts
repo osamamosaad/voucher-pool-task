5bbfb70ed3e1de62203325cbc2cd2adc84db9c30
Osama Mosaad <osamamosaad14@gmail.com>
2023-09-29 22:13:26 +0300

    initial commit

---
+++ app/src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import typeorm from './config/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { CustomerModule } from './customer/customer.module';
+import { VoucherModule } from './voucher/voucher.module';
+
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [typeorm]
+    }),
+    // TypeOrmModule.forRootAsync({
+    //   inject: [ConfigService],
+    //   useFactory: async (configService: ConfigService) => (configService.get('typeorm'))
+    // }),
+    CustomerModule,
+    VoucherModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ app/src/app.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+import { config as dotenvConfig } from 'dotenv';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    dotenvConfig({ path: '.env' });
+    return 'Hello World! in nestjs ' + __dirname + "../";
+  }
+}
